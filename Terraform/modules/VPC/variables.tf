afe5524b7fcb25a7fa839689b87f912183e96496
shashikanthmartha <108862566+shashikanthmartha@users.noreply.github.com>
2024-09-12 22:31:37 +0530

    Update variables.tf

--- Terraform/modules/VPC/variables.tf
+++ Terraform/modules/VPC/variables.tf
@@ -2,12 +2,31 @@ variable "vpc_cidr" {
   description = "VPC CIDR Range"
   type = string
 }
-variable "subnet_cidr" {
+
+variable "subnet_cidr_public" {
     description = "Subnet CIDRS"
     type = list(string)
 }
-variable "subnet_names" {
+variable "subnet_cidr_private" {
+    description = "Subnet CIDRS"
+    type = list(string)
+}
+variable "subnet_cidr_batabase" {
+    description = "Subnet CIDRS"
+    type = list(string)
+}
+variable "subnet_names_public" {
     description = "Subnet names"
     type = list(string)
     default = [ "PublicSubnet1", "PublicSubnet2" ]
+}
+variable "subnet_names_private" {
+    description = "Subnet names"
+    type = list(string)
+    default = [ "privateSubnet1", "privateSubnet2" ]
+}
+variable "subnet_names_database" {
+    description = "Subnet names"
+    type = list(string)
+    default = [ "DBSubnet1", "DBSubnet2" ]
 }
