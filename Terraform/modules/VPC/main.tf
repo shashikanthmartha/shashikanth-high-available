e06a3e5b3857c2c52b7b86e21da940653c51fa45
shashikanthmartha <108862566+shashikanthmartha@users.noreply.github.com>
2024-09-12 21:47:59 +0530

    Update main.tf

--- Terraform/modules/VPC/main.tf
+++ Terraform/modules/VPC/main.tf
@@ -1,1 +1,26 @@
 
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  count = 2
+  vpc_id = aws_vpc.main.id
+  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 4, count.index)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private" {
+  count = 4
+  vpc_id = aws_vpc.main.id
+  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 4, count.index + 2)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index % 2)
+}
+
+resource "aws_subnet" "database" {
+  count = 2
+  vpc_id = aws_vpc.main.id
+  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 4, count.index + 6)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+}
