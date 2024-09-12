2141228a4b34200d0ccdedae372473366e84d837
shashikanthmartha <108862566+shashikanthmartha@users.noreply.github.com>
2024-09-12 22:32:18 +0530

    Create main.tf

---
+++ Terraform/main.tf
@@ -0,0 +1,7 @@
+module "vpc" {
+  source = "./modules/vpc"
+  vpc_cidr = var.vpc_cidr
+  subnet_cidr_public = var.subnet_cidr_public
+  subnet_cidr_private = var.subnet_cidr_private
+  subnet_cidr_batabase = var.subnet_cidr_batabase
+}
