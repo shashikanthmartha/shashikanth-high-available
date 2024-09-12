8d5bc43a5ce25ba6c3a13bd993ccb217746d1f66
shashikanthmartha <108862566+shashikanthmartha@users.noreply.github.com>
2024-09-12 22:44:41 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "./modules/vpc"
+  source = "./modules/VPC"
   vpc_cidr = var.vpc_cidr
   subnet_cidr_public = var.subnet_cidr_public
   subnet_cidr_private = var.subnet_cidr_private
