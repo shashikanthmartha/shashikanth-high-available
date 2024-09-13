3d404c0d5edabf77d926e7bc91077a5379482641
shashikanthmartha <108862566+shashikanthmartha@users.noreply.github.com>
2024-09-13 15:38:34 +0530

    Update variables.tf

--- Terraform/modules/RDS/variables.tf
+++ Terraform/modules/RDS/variables.tf
@@ -1,35 +1,27 @@
-variable "cluster_identifier" {
-  description = "The name of the RDS cluster"
-  type = string
-   default = "my-cluster"
-}
-variable "engine" {
-  description = "The name of the database engine to be used for this DB cluster"
-  type = string
-   default = "mysql"
+  default = "mysql"
 }
 variable "engine_version" {
   description = "The database engine version"
   type = string
-   default = "5.7.19"
+  default = "5.7.19"
 }
 variable "availability_zones" {
-    description = "The availability zones"
-    type = list(string)
+  description = "The availability zones"
+  type = list(string)
 }
 variable "database_name" {
-    description = "The name for your database"
-    type = string
-     default = "mydb"
+  description = "The name for your database"
+  type = string
+  default = "mydb"
 }
 variable "master_username" {
-    description = "The master username"
-    type = string
-     default = "admin"
+  description = "The master username"
+  type = string
+  default = "admin"
 }
 variable "master_password" {
-    description = "The master password"
-    type = string
-     default = "password"
+  description = "The master password"
+  type = string
+  default = "password"
   
 }
