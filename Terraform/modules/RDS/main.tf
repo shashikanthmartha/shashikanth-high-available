9d46973074dfb85eb9a61eaf0fa73823a7c3b210
shashikanthmartha <108862566+shashikanthmartha@users.noreply.github.com>
2024-09-13 15:00:15 +0530

    Update main.tf

--- Terraform/modules/RDS/main.tf
+++ Terraform/modules/RDS/main.tf
@@ -1,1 +1,11 @@
-
+resource "aws_rds_cluster" "default" {
+  cluster_identifier      = var.cluster_identifier
+  engine                  = var.engine
+  engine_version          = var.engine_version
+  availability_zones      = var.availability_zones
+  database_name           = var.database_name
+  master_username         = var.master_username
+  master_password         = var.master_password
+  backup_retention_period = 5
+  preferred_backup_window = "07:00-09:00"
+}
