3bf8897e9df75420cb9382a9389d76981865f785
shashikanthmartha <108862566+shashikanthmartha@users.noreply.github.com>
2024-09-13 15:40:34 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -15,4 +15,36 @@ variable "subnet_cidr_batabase" {
     description = "Subnet database CIDRS"
     type = list(string)
 }
+#RDS variables
+variable "cluster_identifier" {
+  description = "The name of the RDS cluster"
+  type = string
+   
+}
 
+variable "engine" {
+  description = "The name of the database engine to be used for this DB cluster"
+  type = string
+  
+}
+variable "engine_version" {
+  description = "The database engine version"
+  type = string
+   
+}
+
+variable "database_name" {
+    description = "The name for your database"
+    type = string
+    
+}
+variable "master_username" {
+    description = "The master username"
+    type = string
+     
+}
+variable "master_password" {
+    description = "The master password"
+    type = string
+ 
+}
