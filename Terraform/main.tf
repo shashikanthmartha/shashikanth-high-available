a6d89907df542bbec8d223b0262f59e78230ea33
shashikanthmartha <108862566+shashikanthmartha@users.noreply.github.com>
2024-09-13 15:03:31 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -5,3 +5,13 @@ module "vpc" {
   subnet_cidr_private = var.subnet_cidr_private
   subnet_cidr_batabase = var.subnet_cidr_batabase
 }
+module "name" {
+  source = "./modules/RDS"
+  cluster_identifier = var.cluster_identifier
+  engine = var.engine
+  engine_version = var.engine_version
+  availability_zones = module.vpc.database_subnets
+  database_name = var.database_name
+  master_username = var.master_username
+  master_password = var.master_password
+}
