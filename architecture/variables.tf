447c1973a945f311d1f3b894dc162f241c77f92a
shashikanthmartha <108862566+shashikanthmartha@users.noreply.github.com>
2024-09-20 19:57:19 +0530

    Update variables.tf

--- architecture/variables.tf
+++ architecture/variables.tf
@@ -27,7 +27,6 @@ variable "public_subnets" {
 variable "private_subnets" {
   description = "Map of private subnets"
   type = map(string)
-  
 }
 variable "rds_conf" {
   type    = any
@@ -40,11 +39,7 @@ variable "rds_sg_egress_rules" {
   type = any
 }
 
-variable "local_ssm_policies" {
-  description = "The policies to attach to the SSM role"
-  type        = map(string)
-  
-}
+
 variable "ami_id" {
   description = "The AMI ID to use for the launch template"
   type        = string  
@@ -55,43 +50,16 @@ variable "instance_type" {
   type        = string
   
 }
-variable "auto_private_subnets" {
-  description = "The private subnets to use for the launch template"
-  type        = list(object({
-    id = string
-  }))
-  
-}
-variable "auto_public_subnets" {
-  description = "The public subnets to use for the launch template"
-  type        = list(object({
-    id = string
-  }))
-  
-}
-variable "vpc_id" {
-  description = "The VPC ID to use for the launch template"
-  type        = string
-  
-}
-variable "aws_acm_certificate_cert_arn" {
-  description = "The ARN of the ACM certificate to use for the ALB"
-  type        = string  
-  
-}
+
+
+
 variable "alb_sg_ingress_rules" {
     type=any
 }
 variable "alb_sg_egress_rules" {
   type = any
 }
-variable "user_data" {
-  description = "The user data to use for the launch template"
-  type        = object({
-    rendered = string
-  })
-  
-}
+
 variable "ec2_sg_ingress_rules" {
   type = any
   
