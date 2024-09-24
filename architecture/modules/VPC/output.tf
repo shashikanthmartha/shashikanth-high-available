f49065c6a2cb6bd4632ac37f7d53fad70b80aff6
shashikanthmartha <108862566+shashikanthmartha@users.noreply.github.com>
2024-09-24 14:31:24 +0530

    Delete architecture directory

--- architecture/modules/VPC/output.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-output "public_subnets" {
-  value = aws_subnet.public.*.id
-}
-output "private_subnets" {
-  value = zipmap(aws_subnet.private.*.id, aws_subnet.private.*.id)
-}
--- architecture/modules/nat_gateway/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "public_subnets" {
-  description = "Map of public subnets"
-  type = map(string)
-}
-variable "env" {
-  type = string
-}
